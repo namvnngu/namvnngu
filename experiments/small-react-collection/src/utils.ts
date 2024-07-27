db7107c1aa13d432ea07a8b35415498868d89019
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-07-28 00:40:04 +1000

    experiments: add useCounter hook for small-react-collection

--- experiments/small-react-collection/src/utils.ts
+++ experiments/small-react-collection/src/utils.ts
@@ -38,4 +38,24 @@ function formatDate(date: Date, locale: string): string {
   });
 }
 
-export { classes, getRandomColor, formatDate };
+function clamp(
+  value: number,
+  min: number | undefined,
+  max: number | undefined,
+): number {
+  if (min === undefined && max === undefined) {
+    return value;
+  }
+
+  if (min !== undefined && max === undefined) {
+    return Math.max(value, min);
+  }
+
+  if (min === undefined && max !== undefined) {
+    return Math.min(value, max);
+  }
+
+  return Math.min(Math.max(value, min!), max!);
+}
+
+export { classes, getRandomColor, formatDate, clamp };
